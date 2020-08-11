46e450515623b678284e3db7f35701218aaabe24
Ana Inés <68863891+anaines-21q@users.noreply.github.com>
2020-08-10 21:57:36 -0300

    Update login.js
    funciones para guardar usuario en storage data y para corroborar login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+// Name and Password from the register-form
+var newemail = document.getElementById('newemail');
+var newpassword = document.getElementById('newpassword');
 
-});
+// storing input from register-form (key , value)
+function store() {
+    localStorage.setItem(newemail.value, newpassword.value);
+ alert("Quedaste registrado!");}
+
+// check if stored data from register-form is equal to entered data in the login-form
+function usuario() {
+
+ // entered data from the login-form
+ var email = document.getElementById("emaillogin").value;
+ 
+ var password = document.getElementById('password').value;
+
+// stored data from the register-form
+var pwd = localStorage.getItem(email);
+
+// check if stored data from register-form is equal to data from login form
+if (pwd != null && (pwd == password))
+ {
+    window.location.href="homepage.html";
+    }else {
+        alert('nooooo tenes usuario!');
+    }
+}
