4a18ddc51767bd53c0e9eed2aea42dd8b8999a02
anaines-21q <anaines.21q@gmail.com>
2020-09-01 21:51:17 -0300

    comments
    comments

---
+++ js/comments.js
@@ -0,0 +1,45 @@
+var Comments = {};
+
+function showCommentsList(Array){
+   let htmlContentToAppend = "";
+  
+    for(let i = 0; i < Array.length; i++){
+        let comments = Array[i];
+       
+        htmlContentToAppend += `
+          <br/>
+<li class="media">
+<div class="media-body">
+<label class="mt-0">`+ comments.user + `
+<span class="mute">`+ comments.dateTime +`</span>
+<span>`+ comments.score +`</span>
+</label>
+<br/>
+<label class="small">`+ comments.description +`</label>
+<hr/>
+</div>
+</li>
+
+    `
+    
+       document.getElementById("com-list-container").innerHTML = htmlContentToAppend;
+  
+  
+  }}
+  
+ 
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {comments = resultObj.data;
+                           
+            showCommentsList(comments);
+        }
+    });
+});
+
+
