1d137c1b284528ef7c120526bafd611344c0026d
Ana Inés <68863891+anaines-21q@users.noreply.github.com>
2020-10-22 19:47:28 -0300

    dinamic

--- js/cart.js
+++ js/cart.js
@@ -1,37 +1,110 @@
-let input0= 0
-let input1= 0
 let envioPercentage = 0.05;
 var currentProductsArray = [];
-let Cotizacion = 40; 
+let Cotizacion = 40;
+
+var currency=[]
+var precio=[]
+var cantidad=[]
+
+var subtotal = 0;
+var valorenvio = 0;
+var total = 0;
+
+
 
 function showArticlesList(currentArticlesArray){
     let htmlContentToAppend = "";
-   
     for(let i = 0; i < currentArticlesArray.length; i++){
     let articulo = currentArticlesArray[i];
-   
- 
     {
-        htmlContentToAppend += `
-        <tr>
-        <td class="col-3"><img src="` + articulo.src + `" alt="`+ articulo.name+ `" class="img-thumbnail"> </td>
-        <td class="col-3">`+ articulo.name +`</td>
-        <td class="col-3">`+articulo.currency+``+" "+``+articulo.unitCost+`</td>
-        <td class="col-3">
-        <input type="number" class="form-control" id="input`+i+`" placeholder="" required="" value="1" min="0"></td>
-        <td class="col-3"><span id="subtotal`+i+`"></span></td>
-        </tr>
-        
-        `
-    
-        }
-
-        
-        document.getElementById("cart-list-container").innerHTML = htmlContentToAppend;
+    htmlContentToAppend += `
+    <tr class="prod">
+    <td class="col-3"><img class="img-thumbnail rounded float-left" style="max-width:30%" src="` + articulo.src + `" alt="`+ articulo.name+ `" ></td>
+    <td class="col-4">`+ articulo.name +`</td>
+    <td class="col-3">`+articulo.currency+``+" "+``+articulo.unitCost+`</td>
+    <td class="col-3">
+    <input type="number" class="input" placeholder="" required="" value="1" min="0"></td>
+    <td class="col-3"><span class="subtotalitem">` + articulo.currency + " " + (articulo.unitCost * articulo.count) + `</span></td>
+    <td class="col-3"><button type="button" class="btn btn-danger eliminar "  id= "eliminar`+ i +`">Eliminar articulo</button></td>
+    </tr>
+    `
     }
+    
+    document.getElementById("cart-list-container").innerHTML = htmlContentToAppend;
+    
+    cantidad[i] = articulo.count;
+    currency[i] = articulo.currency;
+    if (currency[i] == "USD") {
+    precio[i] = articulo.unitCost;
+    subtotal += precio[i] * cantidad[i] * Cotizacion;
+    } else {
+    precio[i] = articulo.unitCost;
+    subtotal += precio[i] * cantidad[i];
+    }
+    
+    document.getElementById("prodxcant").innerHTML = "UYU " + subtotal;
+    
+    updateTotalCosts();
+    
+    }
+    let eliminar = document.getElementsByClassName("eliminar");
+    let prod = document.getElementsByClassName("prod");
+    let subtotalitem = document.getElementsByClassName("subtotalitem");
+    for (let i = 0; i < prod.length; i++) {
+    prod[i].addEventListener("change", function () {
+    cantidad[i] = document.getElementsByClassName("input")[i].value;
+    subtotalitem[i].innerHTML = currency[i] + " " + (precio[i] * cantidad[i]);
+    
+    subtotal = 0;
+    for (h = 0; h < prod.length; h++) {
+    if (currency[h] == "USD") {
+    subtotal += Math.round(precio[h] * cantidad[h] * Cotizacion);
+    } else {
+    subtotal += Math.round(precio[h] * cantidad[h]);
+    }
+    }
+
+
+    
+    document.getElementById("prodxcant").innerHTML = "UYU " + subtotal;
+    updateTotalCosts()
+    
+    
+
+
+        
+      
+            
+
+
+    })
+    
+    let eliminar = document.getElementsByClassName("eliminar");
+
+    eliminar[i].addEventListener("click", function(){
+        
+        let prod = document.getElementsByClassName("prod");
+        prod[i].remove()
+        delete articulo.articles[i]
+        
+        updateTotalCosts()
+    })};
+
+
 }
+    
+    
+    function updateTotalCosts() {
+     valorenvio = Math.round(subtotal * envioPercentage);
+    document.getElementById("porenvio").innerHTML = "UYU " + valorenvio;
+    total = subtotal + valorenvio;
+    document.getElementById("total").innerHTML = "UYU " + total;
+    
+    }
+    
 
-
+   
+    
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_BUY_URL_DES).then(function(resultObj){
@@ -42,16 +115,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     
 
             showArticlesList(articulo.articles);
-            document.getElementById("input0").addEventListener("change", function(){
-                q1 = this.value;
-                updateTotalCosts();
-            });
-            
-                document.getElementById("input1").addEventListener("change", function(){
-                    q2 = this.value;
-                    updateTotalCosts();
-                });
-
+                
                 document.getElementById("premiumradio").addEventListener("change", function(){
                     envioPercentage = 0.15;
                     updateTotalCosts();
@@ -68,42 +132,118 @@ document.addEventListener("DOMContentLoaded", function(e){
                 });
 
 
+                
 
-
-
+                
         }
     });
 });
 
-/**
- * Convertir valor en dolares a pesos
- * @param {*} cost 
- */
-function convertToPesos(cost){
-    return cost * VALOR_CONVERSION;
+
+  function formm(){
+    var forma = document.getElementsByName("forma");
+    let contentAppend = ''
+    let form=''
+    
+    for(var i = 0; i < forma.length; i++) {
+        if(forma[i].checked)
+           selectedforma = forma[i].value;};
+           
+
+           contentAppend += `
+          
+           <h6>Tu Seleccion:`+ selectedforma  +`</h6>
+   
+      `
+      document.getElementById("spanforma").innerHTML = contentAppend;
+
+    if (selectedforma == "Tarjeta de Credito"){
+
+
+   form+=`
+          
+   <hr class="mb-4">
+    <h5 class="mb-3">Datos Tarjeta de Credito</h5>
+    <div class="form-group">
+      <label for="nrotarjeta">Numero de tarjeta</label>
+      <input type="number" class="form-control" id="Nrotarjeta" placeholder="XXXXXXXXXXXXXXXX" required>
+    </div>
+    <div class="form-group">
+      <label for="vto">Vto.</label>
+      <input class="form-control" id="vto" placeholder="XX/XXXX" >
+      <label for="cvv">CVV</label>
+      <input type="number" class="form-control" id="cvv" placeholder="XXX" required>
+    </div>
+    
+`
+
+document.getElementById("form").innerHTML = form;}
+
+else if (selectedforma == "Transferencia Bancaria"){
+
+    form+=`
+           
+    <hr class="mb-4">
+     <h5 class="mb-3">Datos cuenta Bancaria</h5>
+     <div class="form-group">
+       <label for="inputAddress">Numero de cuenta</label>
+       <input type="number" class="form-control" id="Nrocuenta" placeholder="numero cuenta" required>
+     </div>
+     
+     
+ `
+ 
+ document.getElementById("form").innerHTML = form;}}
+ 
+ 
+
+
+function pago(){
+    var forma = document.getElementsByName("forma");
+    
+    for(var i = 0; i < forma.length; i++) {
+        if(forma[i].checked)
+           selectedforma = forma[i].value;};
+
+if (selectedforma === "Tarjeta de Credito"){
+ nrotarjeta= document.getElementById("Nrotarjeta").value
+ vto = document.getElementById("vto").value 
+ cvv = document.getElementById("cvv").value 
+ 
+ if (nrotarjeta == null || nrotarjeta.length == 0)
+ {alert ("el numero de tarjeta es incorrecto")}
+
+ if(cvv == null || cvv.length == 0)
+
+ {alert ("Ingresa o verifica el numero de control de validacion correctamente")}
+
+
+if (vto == null || vto.length == 0 ){
+    {alert ("ingresa vencimiento correctamente")}
 }
 
-function updateTotalCosts(){
- 
+    
+}
 
- let subtotalArticulo1HTML = document.getElementById("subtotal0");
- let subtotalArticulo2HTML = document.getElementById("subtotal1");
- let SubtotalArticulosHTML= document.getElementById("prodxcant");
- let valorEnvioHTML = document.getElementById("porenvio")
- let totalCostHTML = document.getElementById("total");
- 
+if (selectedforma === "Transferencia Bancaria"){
+    nroCuenta = document.getElementById("Nrocuenta").value
+    if (nroCuenta == null || nroCuenta.length ==0)
+    {alert("ingresa numero de cuenta correctamente")} 
+}
+else {
+    alert ("No has ingresado forma de pago")
+    
+}
+}
 
-
-let subtotalArticulo1Show = articulo.articles[0].unitCost * document.getElementById("input0").value;
-let subtotalArticulo2Show = articulo.articles[1].unitCost * document.getElementById("input1").value;
-let subtotalArticulosShow = subtotalArticulo1Show + (subtotalArticulo2Show * Cotizacion)
-let valorEnvioShow = Math.round(subtotalArticulosShow * envioPercentage)
-let totalCostShow = Math.round(subtotalArticulosShow * envioPercentage)*100
-
-
-    subtotalArticulo1HTML.innerHTML = "UYU" + " " + subtotalArticulo1Show
-    subtotalArticulo2HTML.innerHTML = "USD" + " " + subtotalArticulo2Show
-    SubtotalArticulosHTML.innerHTML = "UYU" + subtotalArticulosShow
-    valorEnvioHTML.innerHTML = "UYU" + valorEnvioShow
-    totalCostHTML.innerHTML = "UYU" + totalCostShow 
+function hola(){
+    var forma = document.getElementsByName("forma");
+    
+    for(var i = 0; i < forma.length; i++) {
+        if(forma[i].checked)
+           selectedforma = forma[i].value;};
+console.log(selectedforma)
+    if (selectedforma = undefined || selectedforma==null || selectedforma==0){
+       return false
+    }
 }
